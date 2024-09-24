a8e80915446c04cd5d063e726e07249dc6495474
LeonidasVS <villafrancosibrian@gmail.com>
2024-09-23 21:28:53 -0600

    Guia #1 Entity: Obtener clientes y buscar por ID

---
+++ EFDemo/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EFDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnObtener = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscador = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(585, 300);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnObtener
+            // 
+            this.btnObtener.Location = new System.Drawing.Point(463, 348);
+            this.btnObtener.Name = "btnObtener";
+            this.btnObtener.Size = new System.Drawing.Size(146, 25);
+            this.btnObtener.TabIndex = 1;
+            this.btnObtener.Text = "CargarDatos";
+            this.btnObtener.UseVisualStyleBackColor = true;
+            this.btnObtener.Click += new System.EventHandler(this.btnObtener_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(142, 346);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscador
+            // 
+            this.txtBuscador.Location = new System.Drawing.Point(34, 348);
+            this.txtBuscador.Name = "txtBuscador";
+            this.txtBuscador.Size = new System.Drawing.Size(100, 20);
+            this.txtBuscador.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 399);
+            this.Controls.Add(this.txtBuscador);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnObtener);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = resources.GetString("$this.Text");
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnObtener;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscador;
+    }
+}
+
---
+++ EFDemo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private CustomerRepository customerRepo = new CustomerRepository();
+        private void btnObtener_Click(object sender, EventArgs e)
+        {
+            var cliente = customerRepo.ObtenerTodos();
+
+            dataGridView1.DataSource = cliente;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            var cliente = customerRepo.ObtenerPorID(txtBuscador.Text);
+            List<Customers> listado = new List<Customers>
+            {
+                cliente
+            };
+
+            dataGridView1.DataSource = listado;
+
+        }
+    }
+}
